a16185382a2cd7c7ffcd799a22068eeaa13765fa
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-07-16 21:20:43 -0300

    update t toolbar

--- src/@vex/layout/toolbar/toolbar.module.ts
+++ src/@vex/layout/toolbar/toolbar.module.ts
@@ -6,7 +6,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatRippleModule } from '@angular/material/core';
-import { ToolbarNotificationsModule } from './toolbar-notifications/toolbar-notifications.module';
 import { ToolbarUserModule } from './toolbar-user/toolbar-user.module';
 import { IconModule } from '@visurel/iconify-angular';
 import { NavigationModule } from '../navigation/navigation.module';
@@ -25,7 +24,6 @@ import { ContainerModule } from '../../directives/container/container.module';
     MatIconModule,
     MatMenuModule,
     MatRippleModule,
-    ToolbarNotificationsModule,
     ToolbarUserModule,
     IconModule,
     NavigationModule,
